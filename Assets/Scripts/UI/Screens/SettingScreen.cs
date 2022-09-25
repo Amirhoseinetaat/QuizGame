5f5c9544aee9b88d12692c125ef53bf37961c89e
Amirhossein <ae010t@gmail.com>
2022-09-25 12:31:22 -0400

    [init] [added] added the project folders and libraries

---
+++ Assets/Scripts/UI/Screens/SettingScreen.cs
@@ -0,0 +1,19 @@
+using GameWarriors.UIDomain.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.Screens
+{
+    public class SettingScreen : UIScreenItem
+    {
+        public const string SCREEN_NAME = "SettingScreen";
+        public override string ScreenName => SCREEN_NAME;
+
+        public void SetData(string data)
+        {
+            
+        }
+    }
+}
